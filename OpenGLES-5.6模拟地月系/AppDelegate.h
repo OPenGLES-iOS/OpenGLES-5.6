a80326aad77e11b00df2856b0b4746aca5f53e01
Chenshiwen <cswzmg@live.com>
2017-05-23 16:05:36 +0800

    Initial Commit

---
+++ OpenGLES-5.6模拟地月系/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OpenGLES-5.6模拟地月系
+//
+//  Created by ShiWen on 2017/5/23.
+//  Copyright © 2017年 ShiWen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ OpenGLES-5.6模拟地月系/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OpenGLES-5.6模拟地月系
+//
+//  Created by ShiWen on 2017/5/23.
+//  Copyright © 2017年 ShiWen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
